16d9f67034885470b2201d262126f13d6f9edbdd
devallever <devallever@163.com>
2022-08-07 18:10:54 +0800

    App模板

---
+++ app/src/main/java/app/allever/android/learning/audiovideo/MainActivity.kt
@@ -0,0 +1,20 @@
+package app.allever.android.learning.audiovideo
+
+import app.allever.android.learning.audiovideo.databinding.ActivityMainBinding
+import app.allever.android.lib.common.BaseActivity
+import app.allever.android.lib.mvvm.base.BaseViewModel
+import app.allever.android.lib.mvvm.base.MvvmConfig
+
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+
+    override fun init() {
+        initTopBar("Android模板", showBackIcon = false)
+    }
+
+    override fun getContentMvvmConfig() = MvvmConfig(R.layout.activity_main, BR.mainVM)
+}
+
+class MainViewModel : BaseViewModel() {
+    override fun init() {
+    }
+}
